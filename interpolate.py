8c6c848973e67dcb9ca1981674de323ff3bb40fd
Joshua Keough <jak2248@columbia.edu>
2017-04-25 19:46:27 -0400

    interpolation on datetime

---
+++ interpolate.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import sys
+
+def main():
+
+	input_file_name = sys.argv[1]
+	#pass in what you want the new name to be
+	output_file_name = sys.argv[2]
+
+	header = ["COMMON NAME", "LATITUDE", "LONGITUDE", "OBSERVATION COUNT","OBSERVATION DATE", "temperature", "time_deltas"]
+	df = pd.read_csv(input_file_name, names = header, skiprows = 1)
+	common_name = df.get_value(1, 'COMMON NAME')
+
+	#change OBSERVATION DATE to date-time-index
+	df['DATETIME'] = pd.to_datetime(df['OBSERVATION DATE'])
+	#df['DATETIME'] = pd.DatetimeIndex(df['OBSERVATION DATE'])
+	df = df.set_index(pd.DatetimeIndex(df['DATETIME']))
+	
+
+	df = df.resample('D').mean()
+	df = df.interpolate(method = 'time')
+
+	#readd common name column
+	df['COMMON NAME'] = common_name
+
+	df.to_csv(output_file_name)
+
+if __name__ == "__main__":
+	main()
